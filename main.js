eef908214771b76d9ef7ebd38f076be47a1f5843
Xenostar <157074608+Xenostar1337@users.noreply.github.com>
2024-03-06 23:35:34 -0700

    alpha release

--- main.js
+++ main.js
@@ -4,68 +4,74 @@ const bookForm = document.querySelector(".bookForm");
 const submit = document.querySelector(".submit");
 const bookContainer = document.querySelector(".bookArea");
 let divInstance = 0;
-let delInstance = 0;
-let readInstance = 0;
-let uID = "";
-
-
 
 function book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-      return(title + " by " + author + ", " + pages + " pages, " + read);
-    }
-    myLibrary.push(this);    
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return(title + " by " + author + ", " + pages + " pages, " + read);
   }
-function createUniqueDiv(target) {          
+  myLibrary.push(this);    
+}
+
+function createUniqueID(target) {          
     return target+divInstance;       
-  }
-function createUniqueDel(target) {          
-    return target+divInstance;       
-  }
-function createUniqueRead(target) {          
-    return target+divInstance;       
-  }
-function readBook() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    //console.log(myLibrary[i].read)
-  }
-  }
+}
 
-  function addBook() {    
-    let newDiv = document.createElement('div');
-    let newBG = document.createElement('div');
-    let newDel = document.createElement('button');
-    let newRead = document.createElement('button');
-    newRead.textContent = 'Read';
-    newDel.textContent = 'Remove';  
-    newRead.id = createUniqueRead("newRead");  
-    newDel.id = createUniqueDel("newDel"); 
-    newDiv.id = createUniqueDiv("newDiv");
-    divInstance++;  
-    newRead.className = "readIt";
-    newDel.className = 'delButton';   
-    newDiv.className = 'book';
-    newBG.className = 'bookBG';       
-    document.querySelector(".bookArea").appendChild(newBG);
-    newBG.appendChild(newDiv);
-
-
-
-    newRead.addEventListener('click', readAbook);
-    newDel.addEventListener('click', delBook);
-  
-    return { newBG, newDiv, newDel, newRead };
-  }
-  
-
-function newBook() {    
+function showForm() {    
   bookForm.style.display = "grid";
   addButton.style.display = "none";
   submit.style.display = 'block';    
+}
+
+function populate() {
+  let parentDiv = document.querySelector(".bookArea");
+  while (parentDiv.firstChild) {
+    parentDiv.removeChild(parentDiv.firstChild);
+  }
+  for (let i = 0; i < myLibrary.length; i++) {   // for each index in library, build a card.
+    let newBG = document.createElement('div');
+    let newDiv = document.createElement('div');
+    let newDel = document.createElement('button');
+    let newRead = document.createElement('button');
+    newRead.textContent = 'Read This';
+    newDel.textContent = 'Remove';
+    newDiv.textContent = myLibrary[i].title;  
+    newRead.id = createUniqueID("newRead");  
+    newDel.id = createUniqueID("newDel"); 
+    newDiv.id = createUniqueID("newDiv");
+    newBG.id = createUniqueID("newBG")
+    newRead.className = "readIt";
+    newDel.className = 'delButton';   
+    newDiv.className = 'book';
+    newBG.className = 'bookBG';     
+    document.querySelector(".bookArea").appendChild(newBG);
+    newBG.appendChild(newDiv);
+    newDiv.appendChild(newRead);   
+    newDiv.appendChild(newDel);
+    console.log(myLibrary.length);    
+    newRead.addEventListener('click', readAbook);
+    newDel.addEventListener('click', delBook);  
+    divInstance++; 
+    if (myLibrary[i].read === true) {
+      let idElement = "newRead"+ i;  
+      document.getElementById(idElement).style.display = "none";
+    }           
+  }
+  if (myLibrary.length > 0) {
+    bookContainer.style.display = "grid";
+  } else {
+    bookContainer.style.display = "none";
+  }
+  console.log(myLibrary);
+}
+
+function rePopulate() {
+      populate();
+     divInstance = 0;        
+  
 }
 
 function submitBook() {
@@ -83,42 +89,25 @@ function submitBook() {
   bookForm.style.display = "none";
   submit.style.display = 'none';
   addButton.style.display = "block";
-  listAll(myLibrary);   
-  divInstance = 0;
-  delInstance = 0;
-  readInstance = 0;
+  rePopulate();   
   }
 }
 
-  function listAll(myLibrary) {
-    let parentDiv = document.querySelector(".bookArea");
-    while (parentDiv.firstChild) {
-      parentDiv.removeChild(parentDiv.firstChild);
-    }
-    for (let i = 0; i < myLibrary.length; i++) {
-      let { newBG, newDiv, newDel, newRead } = addBook();
-      let idElement =  "newRead"+ i;          
-      newDiv.textContent = myLibrary[i].title;
-      newDiv.appendChild(newRead);           
-      newDiv.appendChild(newDel);      
-      if (myLibrary[i].read === true) {
-        let hasRead = document.getElementById(idElement);
-        hasRead.style.display = "none";
-      }             
-    }
-    if (myLibrary.length > 0) {
-      bookContainer.style.display = "grid";
-    }
-  }
-
-function readAbook() {
-  console.log("read a book", )
+function readAbook(buttonID) {
+  let element = buttonID.target.id;  
+  let result = element.replace(/\D/g, '');  
+  myLibrary[result].read = true;//change status in myLibrary
+  rePopulate();
 }
-  
-//newBG.remove();
 
+function delBook(buttonID) {
+  let element = buttonID.target.id;  
+  let result = element.replace(/\D/g, '');
+  myLibrary.splice(result , 1);
+  rePopulate();
+}
 
-  listAll(myLibrary);
-  
-  addButton.addEventListener("click", newBook);
-  submit.addEventListener("click", submitBook);
+const riven = new book ("The Rise of Riverstone", "Mandy Schimelpfenig", 495, false);
+rePopulate();
+addButton.addEventListener("click", showForm);
+submit.addEventListener("click", submitBook);
